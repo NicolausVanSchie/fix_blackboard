4c5aad7ab2540b15cbbb1cec68f6362d56f1e22e
NicolausVanSchie <Nicko21.nvs@gmail.com>
2016-01-08 00:05:48 +1000

    Error checking, better matching

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
-var link = document.getElementsByClassName("brandingImgWrap")[0].getElementsByTagName('a')[0];
-link.setAttribute('href', 'https://learn.uq.edu.au');
-link.removeAttribute('target');
+try {
+        // find the link on the page, not the best way probably to do this
+        var link = document.getElementsByClassName("brandingImgWrap")[0].getElementsByTagName('a')[0];
+        // Change the url to bb home
+        link.setAttribute('href', 'https://learn.uq.edu.au');
+        // Make it so it does not open a new window
+        link.removeAttribute('target');
+}
+catch(err) {} //we don't really care, as not all bb sites have the link
