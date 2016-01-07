52b0c98c3cd360b26cfca7ab787a3276a9e2e166
NicolausVanSchie <Nicko21.nvs@gmail.com>
2016-01-07 20:12:40 +1000

    doot doot

---
+++ content.js
@@ -0,0 +1,3 @@
+var link = document.getElementsByClassName("brandingImgWrap")[0].getElementsByTagName('a')[0];
+link.setAttribute('href', 'https://learn.uq.edu.au');
+link.removeAttribute('target');
